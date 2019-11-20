adec451da523e48711c4dabe3ea5731753defab6
kschmidt2 <k.schmidt2@gmail.com>
2019-11-20 17:07:20 -0500

    updates draft

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
-var calculator = new Vue({
+setTimeout(function(){
+  var calculator = new Vue({
     el: '#calculator',
     data: {
       currentAge: '',
@@ -6,7 +7,7 @@ var calculator = new Vue({
       income: '',
       incomeIncrease: '',
       savingsYesNo: '',
-      currentSavings: '',
+      currentSavings: '0',
       annualGrowth: '',
       retirementIncome: '',
       socialSecurity: '',
@@ -16,8 +17,19 @@ var calculator = new Vue({
     },
     computed: {
       // calculator math goes here
+      savingsGoal: function() {
+        let totalNeeded = this.income * (90-this.retirementAge) * .75,
+            annualAmount = (totalNeeded-this.currentSavings) / (this.retirementAge-this.currentAge),
+            monthlyAmount = annualAmount/12;
+
+        let results = '<p><b>Total needed:</b> $' + totalNeeded.toLocaleString(0) +  '</p><p><b>Annual amount:</b> $' + annualAmount.toLocaleString(0) + '</p><p><b>Monthly savings:</b> $' + monthlyAmount.toLocaleString(0) + '</p>';
+        console.log(monthlyAmount)
+        
+        return '$' + Math.round(monthlyAmount).toLocaleString();
+      }
     },
     methods: {
       // functions go here
     }
   })
+}, 1000)
