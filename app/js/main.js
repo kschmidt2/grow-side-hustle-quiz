b2ed73866b4044ed06546a588b5ef983d548f75e
kschmidt2 <k.schmidt2@gmail.com>
2019-12-11 17:28:13 -0500

    adds checklist with functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,149 +1,144 @@
 // setTimeout(function(){
-  var calculator = new Vue({
-    el: '#calculator',
+  var quiz = new Vue({
+    el: '#quiz',
     data: {
-      currentAge: '',
-      retirementAge: '',
-      baseSalary: '',
-      savingsYesNo: '',
-      currentSavings: '',
-      annualGrowth: '',
-      retirementIncome: '',
-      socialSecurity: '',
-      totalNeeded: '',
-      saveAmount: '',
-      seen: false
+      steadyIncome: '',
+      peoplePerson: '',
+      drive: '',
+      physicalWork: '',
+      skilled: '',
+      fixingThings: '',
+      kids: '',
+      organized: '',
+      comfortHome: '',
+      lovesDogs: '',
+      regularClients: '',
+      menialTasks: '',
+      socialMedia: '',
+      groceryShopper: '',
+
+      showRideShareQuestion: false,
+      carYesNo: '',
+      showAssistantQuestion: false,
+      computerYesNo: '',
+
+      showResults: false
     },
     computed: {
-      // calculator math goes here
-      savingsGoal: function() {
-        // let totalNeeded = this.income * (90-this.retirementAge) * .75,
-        //     annualAmount = (totalNeeded-this.currentSavings) / (this.retirementAge-this.currentAge),
-        //     monthlyAmount = annualAmount/12;
+      // results go here
 
-        // let results = '<p><b>Total needed:</b> $' + totalNeeded.toLocaleString(0) +  '</p><p><b>Annual amount:</b> $' + annualAmount.toLocaleString(0) + '</p><p><b>Monthly savings:</b> $' + monthlyAmount.toLocaleString(0) + '</p>';
-        // console.log(monthlyAmount)
-        
-        // return '$' + Math.round(monthlyAmount).toLocaleString();
-        console.log("hello")
-
-        if (this.currentAge > this.retirementAge) {
-          return "You can't retire in the past"
-        };
-      
-        if (this.retirementAge > 90) {
-          return "Congrats on discovering immortality! Why not just retire right now?"
-        };
-      
-        let lifetimeEarnings = 0,
-            salary = this.baseSalary,
-            yearsToDeath = 90 - this.retirementAge,
-            years = this.retirementAge - this.currentAge,
-            incr = 1.02;
-      
-        for (let i = 0; i < years; i++) {
-          salary = parseFloat((salary * incr).toFixed(2))
-          lifetimeEarnings += salary
-        };
-      
-        let goal = (salary * .75) * yearsToDeath;
-      
-        salary = this.baseSalary;
-      
-        let balance = this.currentSavings,
-            leftover = true,
-            basePct = .01;
-      
-        while(leftover === true && basePct <= 1) {
-          for (let i = 0; i < years; i++) {
-            balance += parseFloat((salary * basePct).toFixed(2))
-            balance += parseFloat((balance * this.annualGrowth).toFixed(2))
-            salary = parseFloat((salary * incr).toFixed(2))
-          };
-      
-          console.log(`${Math.round(basePct * 100)}% of your salary: $${parseFloat(balance)} of $${parseFloat(goal.toFixed(2))}`);
-
-          return `${Math.round(basePct * 100)}% of your salary: $${parseFloat(balance)} of $${parseFloat(goal.toFixed(2))}`
-      
-          if (goal > balance) {
-            salary = this.baseSalary
-            balance = this.currentSavings
-            basePct += .01
-          } else {
-            salary = this.baseSalary
-            balance = this.currentSavings
-            basePct -= .001
-            leftover = false
-          }
-        };
-      
-        if (basePct < 1){
-          return Math.round(basePct * 100)
-        } else {
-          return `It is impossible for you to retire at ${this.retirementAge}`
-        }
-      }
     },
     methods: {
       // functions go here
+      calculateResults: function() {
+
+        let sideHustles = {
+          rideShare: 0,
+          tutor: 0,
+          groceryShopper: 0,
+          dogWalker: 0,
+          taskHandler: 0,
+          virtualAssistant: 0
+        };
+
+        if (this.steadyIncome) {
+          sideHustles.rideShare++;
+          sideHustles.tutor+=2;
+          sideHustles.groceryShopper++;
+          sideHustles.dogWalker++;
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.peoplePerson) {
+          sideHustles.rideShare++;
+          sideHustles.tutor++;
+          sideHustles.virtualAssistant++;
+        }
+
+        if (this.drive) {
+          sideHustles.rideShare+=2;
+        }
+
+        if (this.physicalWork) {
+          sideHustles.taskHandler+=2;
+        }
+
+        if (this.skilled) {
+          sideHustles.tutor+=2;
+        }
+
+        if (this.fixingThings) {
+          sideHustles.taskHandler+=2;
+        }
+
+        if (this.kids) {
+          sideHustles.tutor+=2;
+        }
+
+        if (this.organized) {
+          sideHustles.tutor++;
+          sideHustles.groceryShopper++;
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.comfortHome) {
+          sideHustles.tutor++;
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.lovesDogs) {
+          sideHustles.dogWalker+=2;
+        }
+
+        if (this.regularClients) {
+          sideHustles.tutor+=2;
+          sideHustles.dogWalker+=2;
+          sideHustles.taskHandler++;
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.menialTasks) {
+          sideHustles.rideShare+=2;
+          sideHustles.groceryShopper+=2;
+          sideHustles.dogWalker++;
+          sideHustles.taskHandler+=2;
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.socialMedia) {
+          sideHustles.virtualAssistant+=2;
+        }
+
+        if (this.groceryShopper) {
+          sideHustles.groceryShopper+=2;
+        }
+
+        let quizResults = "Ride-share: " + sideHustles.rideShare +  "; Tutor: " + sideHustles.tutor + "; Grocery shopper: " + sideHustles.groceryShopper + "; Dog Walker: " + sideHustles.dogWalker + "; Task handler: " + sideHustles.taskHandler + "; Virtual assistant: " + sideHustles.virtualAssistant;
+
+        let sorted = Object.keys(sideHustles)
+                      .sort(function(a,b){
+                        return sideHustles[b] - sideHustles[a] || 0.5 - Math.random();
+                      });
+
+        console.log(quizResults)
+        console.log(sorted)
+
+        let result = sorted[0];
+        console.log(result)
+
+        if (result !== "rideShare" && result !== "virtualAssistant") {
+          this.showResults = !this.ShowResults;
+          console.log(result);
+        }
+
+        if (result == "rideShare") {
+          this.showRideShareQuestion = !this.showRideShareQuestion;
+        } else if (result == "virtualAssistant") {
+          this.showAssistantQuestion = !this.showAssistantQuestion;
+        } 
+
+        console.log(this.showRideShareQuestion);
+      }
     }
   })
 // }, 1000)
-
-// function savingsRate(baseSalary, annualGrowth, currentAge, retirementAge, currentSavings) {
-//   if (currentAge > retirementAge) {
-//     return "You can't retire in the past"
-//   };
-
-//   if (retirementAge > 90) {
-//     return "Congrats on discovering immortality! Why not just retire right now?"
-//   };
-
-//   let lifetimeEarnings = 0,
-//       salary = baseSalary,
-//       yearsToDeath = 90 - retirementAge,
-//       years = retirementAge - currentAge,
-//       incr = 1.02;
-
-//   for (let i = 0; i < years; i++) {
-//     salary = parseFloat((salary * incr).toFixed(2))
-//     lifetimeEarnings += salary
-//   };
-
-//   let goal = (salary * .75) * yearsToDeath;
-
-//   salary = baseSalary;
-
-//   let balance = currentSavings,
-//       leftover = true,
-//       basePct = .01;
-
-//   while(leftover === true && basePct <= 1) {
-//     for (let i = 0; i < years; i++) {
-//       balance += parseFloat((salary * basePct).toFixed(2))
-//       balance += parseFloat((balance * annualGrowth).toFixed(2))
-//       salary = parseFloat((salary * incr).toFixed(2))
-//     };
-
-//     console.log(`${Math.round(basePct * 100)}% of your salary: $${parseFloat(balance.toFixed(2))} of $${parseFloat(goal.toFixed(2))}`);
-
-//     if (goal > balance) {
-//       salary = baseSalary
-//       balance = currentSavings
-//       basePct += .01
-//     } else {
-//       salary = baseSalary
-//       balance = currentSavings
-//       basePct -= .001
-//       leftover = false
-//     }
-//   };
-
-//   if (basePct < 1){
-//     return Math.round(basePct * 100)
-//   } else {
-//     return `It is impossible for you to retire at ${retirementAge}`
-//   }
-// };
-
-// savingsRate(50000,1.06,25,70,0)
